6451ce38dcced3a04e3776d296dfe4bf624e9120
Aaron Swartz <me@aaronsw.com>
2011-12-08 20:18:00 -0500

    add subjects file. oops

---
+++ subjects.py
@@ -0,0 +1,19 @@
+SUBJECT_DB = {
+  128: "Oppose H.R. 1981, the Internet Snooping Bill",
+  127: "Oppose Rep Grimm's H.R. 2483 -- The Corporate Fiend Protection Act",
+  119: "Oppose Klobuchar's S.978, which would criminalize online streaming",
+  118: "Oppose the PROTECT IP Act!",
+  120: "Oppose the PROTECT IP Act!",
+  132: "Oppose the PROTECT IP Act!",
+  140: "Oppose the PROTECT IP Act!",
+  135: "Please introduce legislation to abolish of death penalty",
+  139: "Oppose Senator Hutchison's resolution of dissaproval",
+  149: "Please oppose the Internet Blacklist Bill -- SOPA, HR 3261",
+  151: "Oppose Sen Hutchison's resolution of disapproval",
+  153: "Oppose SOPA (HR 3261), the Internet blacklist bill",
+  157: "Oppose SOPA (HR 3261), the Internet blacklist bill",
+  163: "Oppose PIPA (S.968) and SOPA (H.3261), the Internet censorship bill",
+  164: "Oppose PIPA (S.968) and SOPA (H.3261), the Internet censorship bill",
+  160: "Oppose PIPA (S.968) and SOPA (H.3261), the Internet censorship bill",
+  158: "Oppose PIPA (S.968) and SOPA (H.3261), the Internet censorship bill"
+}
