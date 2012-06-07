d4ccbf60ee151a191245b39aeb33e10d3675dea3
Naomi Fox <naomi.fox@gmail.com>
2012-06-06 17:39:21 -0700

    updated code for latest data file

--- SendBsdCSV.py
+++ SendBsdCSV.py
@@ -4,7 +4,7 @@ from WriteYourRep import *
 from DataForWriteYourRep import *
 
 
-def bsd_Send_To_Senate(csvfile='demo-dataz.csv', statfile='bsd_Send_To_Senate.log', dryrun=False):
+def bsd_Send_To_Senate(csvfile='demo-dataz.csv', messagefile="noCispaMessage.txt", statfile='bsd_Send_To_Senate.log', dryrun=False):
     '''
     Parse from the blue-state-digital csv file
     '''
@@ -14,13 +14,17 @@ def bsd_Send_To_Senate(csvfile='demo-dataz.csv', statfile='bsd_Send_To_Senate.lo
     writeYourRep = WriteYourRep()
     reader = csv.reader(open(csvfile, 'r'), delimiter=',', quotechar='\"')
     genderassigner = GenderLookup()
+
+    (subject, message) = parseMessageFile(messagefile)
     for row in reader:
         name='unknown'
         state='unknown'
         status = ""
         try:
             #print len(row)
-            (date, email, name, addr1, addr2, zip5, city, message, source, subsource, ip) = row
+            (id, email,first_name,last_name,addr1,city,state,zip5,postal) = row
+            addr2=""
+            #(date, email, name, addr1, addr2, zip5, city, message, source, subsource, ip) = row
             print zip5
             z = ZipLookup()
             state = z.getState(zip5)
@@ -30,10 +34,12 @@ def bsd_Send_To_Senate(csvfile='demo-dataz.csv', statfile='bsd_Send_To_Senate.lo
                 print email
                 i.email=email
             if name:
-                names = name.split()
-                i.fname = "".join(iter(names[0:len(names)-1]))
-                i.lname = names[-1]
-                i.prefix = genderassigner.getPrefix(names[0])                     
+                #names = name.split()
+                #i.fname = "".join(iter(names[0:len(names)-1]))
+                #i.lname = names[-1]
+                i.fname = first_name
+                i.lname = last_name
+                i.prefix = genderassigner.getPrefix(i.fname)                     
                 i.id = name
             if addr1:
                 i.addr1 = addr1
@@ -45,39 +51,62 @@ def bsd_Send_To_Senate(csvfile='demo-dataz.csv', statfile='bsd_Send_To_Senate.lo
                 i.city = city
             if message:
                 i.full_msg = message
+            if subject:
+                i.subject = subject
             sens = writeYourRep.getSenators(state)
             for sen in sens:
                 senname = web.lstrips(web.lstrips(web.lstrips(sen, 'http://'), 'https://'), 'www.').split('.')[0]
+                #print senname
                 if dryrun:
-                    status += senname + ": Not attempted with "+ i.__str__()+"\n"
+                    status += sen + " " + senname + ": Not attempted with "+ i.__str__()+"\n"
                 else:
                     q = writeYourRep.writerep_general(sen, i)
                     status += senname + ": " + writeYourRep.getStatus(q) +", "
         except Exception, e:
             #import traceback; traceback.print_exc()
             status=status + ' failed: ' + e.__str__()
-        file(statfile, 'a').write('%s, %s, "%s"\n' % (name, state, status))
+        file(statfile, 'a').write('%s %s, %s, "%s"\n' % (first_name, last_name, state, status))
 
+
+def parseMessageFile(messageFile):
+    '''
+    example message file:
+    The subject is Vote NO on ACTA
+    This is the body of the email.
+
+    This is more of the body of the email.
+    '''
+    
+    f = open(messageFile, 'r')
+    subject=f.readline()
+    body=f.read()
+    return (subject,body)
 
 def usage():
     import sys
     print "Usages of ", sys.argv[0], ":"
-    print "Normal: " + sys.argv[0] + " csvfile statfile"
-    print "Dryrun: " + sys.argv[0] + "-d csvfile statfile"
+    print "Normal: " + sys.argv[0] + " csvfile messagefile statfile"
+    print "Dryrun: " + sys.argv[0] + "-d csvfile messagefile statfile"
     print "csvfile is of form: "
-    print "date, email, name, addr1, addr2, zip5, city, message, source, subsource, ip"
+    print "id, email,first_name,last_name,addr1,city,state,zip5,postal"
+    print ""
+    print "messagefile is of the form: "
+    print "This is the message subject."
+    print "This is the message body."
+    print "And this is more of the message body."
     
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         csvfile = sys.argv[1]
         statfile = sys.argv[2]
         bsd_Send_To_Senate(csvfile, statfile)
         sys.exit(0)
-    if len(sys.argv) == 4 and sys.argv[1] == '-d': #dry run
+    if len(sys.argv) == 5 and sys.argv[1] == '-d': #dry run
         csvfile = sys.argv[2]
-        statfile = sys.argv[3]
-        bsd_Send_To_Senate(csvfile, statfile, dryrun=True)
+        messagefile = sys.argv[3]
+        statfile = sys.argv[4]
+        bsd_Send_To_Senate(csvfile, messagefile, statfile, dryrun=True)
         sys.exit(0)
     else:
         usage()
