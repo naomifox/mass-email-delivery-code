deb7a0862f29fb9a40af398c3694b72a3e0512fc
Naomi Fox <naomi.fox@gmail.com>
2015-01-29 23:53:59 -0800

    Updated build script

--- utils/build.sh
+++ utils/build.sh
@@ -12,9 +12,14 @@ cd $MYDIR
 
 echo "Updating zip-district data"
 #uncomment the following line to rebuild the district-zip map whenever the congressional session changes
-#last update: session 113
-#./get_zip_district_script_alllines.sh
+#last update: session 114
+# rm scriptlines.txt
+# ./get_zip_district_script_alllines.sh
 # python parseZipToDist.py scriptlines.txt > ../wyr-all-zips-data.txt
-php make_dist_zip_db.php > ../zip_per_dist.tsv
+
+# Lookup district for every zip code
+# php make_dist_zip_db.php > ../zip_per_dist.tsv
+
+
 
 echo "OK"
